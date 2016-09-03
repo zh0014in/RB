53eb6c13adebb2a1b96c157a6997493eebc73647
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2016-09-03 15:45:24 +0800

    Initial Commit

---
+++ Rainbow/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rainbow
+//
+//  Created by 张明敏 on 3/9/16.
+//  Copyright © 2016 张明敏. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
