02db9362a0b9340daa13428297e6a2b19ff46e4d
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2016-09-12 23:42:42 +0800

    Q

--- B.cpp
+++ B.cpp
@@ -13,13 +13,13 @@ using namespace std;
 //    infeasible to have such large table in practice.ı
 //    for programming convenient, we store the whole table in memory.
 #define HT 1048576
-#define RLength 16000
+#define QLength 16000
 unsigned char Word[HT][3];
 unsigned char M[HT][3];
 unsigned char D[HT][3];
 unsigned char *pM;
 unsigned char *pD;
-unsigned char R[RLength][3];
+unsigned int Q[QLength];
 
 unordered_map<unsigned long, unsigned int> HashTable;
 unordered_map<unsigned long, unsigned int>::const_iterator G;
@@ -40,6 +40,37 @@ void printWord(unsigned char m[3])
     cout << hex << m[i] << " ";
   }
   cout << endl;
+}
+
+unsigned int char2int(unsigned char m[3]){
+  unsigned int result = 0;
+  result += m[0] << 16;
+  result += m[1] << 8;
+  result += m[2];
+  return result;
+}
+
+int insert2Q(unsigned char m[3]){
+  if(Q[QLength-1] > 0){
+    return -2;
+  }
+  unsigned int result = char2int(m);
+  for(int i = 0; i < QLength; i++){
+    if(Q[i] > result){
+      for(int j = QLength - 1; j > i; j--){
+        Q[j] = Q[j-1];
+      }
+      Q[i] = result;
+      return 1;
+    }
+    else if(Q[i] == result){
+      return -1;
+    }
+    else{
+      continue;
+    }
+  }
+  return 0;
 }
 
 //----  return the next word to be considered
@@ -186,6 +217,9 @@ int main(int argc, char *argv[])
     Word[i][0] = i & 0xff;
     Word[i][1] = (i >> 4) & 0xff;
     Word[i][2] = (i >> 8) & 0xff;
+  }
+  for(int i = 0; i < QLength; i++){
+    Q[i] = 0;
   }
   //----   Build the table.
   cout << "building the table..." << endl;
